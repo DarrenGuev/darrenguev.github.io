93c2d0a9a8ffb505e93679f4c4adc63563ba165f
Marc Darren <marcdarrenguev08@gmail.com>
2025-12-16 09:18:26 +0800

    refactoring

---
+++ data.js
@@ -0,0 +1,27 @@
+var activities = [
+  {
+    "link": "https://darrenguev.github.io/Iphone-13/",
+    "title": "iPhone 13 Showcase",
+    "activityNumber": "Activity 4",
+    "subTitle": "Showcase iPhone 13.",
+  },
+  {
+    "link": "https://darrenguev.github.io/Schwarzsee-Switzerland/",
+    "title": "Schwarzsee Showcase",
+    "activityNumber": "Activity 5",
+    "subTitle": "Showcase of Schwarzsee, Switzerland.",
+  },
+  {
+    "link": "https://darrenguev.github.io/Gallery/",
+    "title": "Gallery Showcase",
+    "activityNumber": "Activity 6",
+    "subTitle": "One Piece-Strawhat Crew Gallery.",
+  },
+  {
+    "link": "under construction",
+    "title": "DotaPedia Website",
+    "activityNumber": "Activity 7",
+    "subTitle": "DotaPedia Website",
+  },
+];
+
